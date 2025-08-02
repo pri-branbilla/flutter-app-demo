35b2676f04e28c7c1581e71f396151e38ba7e776
Alura VideoOps <Pri Branbilla>
2025-08-02 15:36:26 -0300

    aula 3.1 - widgets de layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projeto_exemplo/widgets/titulo_secao.dart';
+import 'package:projeto_exemplo/screens/widgets_conteudo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,88 +15,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Widgets de conteúdo'),),
-        body: ListView(
-          padding: EdgeInsets.all(16),
-          children: [TituloSecao(titulo: 'Textos'),
-          Text('Texto estilizado', style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),),
-          Text('Texto com estilo padrão', style: TextStyle(
-            fontSize: 18,
-          ),),
-          
-          Divider(),
-          TituloSecao(titulo: 'Imagem'),
-          Image.network(
-            'https://picsum.photos/id/237/200/300',
-            height: 240,
-          ),
-
-          Divider(),
-          TituloSecao(titulo: 'Ícone'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-            Icon(Icons.favorite, color: Colors.red, size:32,),
-            Icon(Icons.star, color: Colors.amber, size:32,),
-            Icon(Icons.settings, color: Colors.blue, size:32,),
-          ],),
-          
-          Divider(),
-          TituloSecao(titulo: 'Elevated button'),
-          ElevatedButton(onPressed: () {}, child: Text('Clique aqui'),),
-          
-          ]
-        ),
-      ),
+      home: ListContents(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class ListContents extends StatelessWidget {
+  const ListContents({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: Text('Widgets de conteúdo')),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => WidgetsConteudo()),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+            child: Text('Widgets de conteúdo'),
+          ),
+        ],
       ),
     );
   }
